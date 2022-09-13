d4886df8d8e47e818cbc3898c2bbfa4fb9456674
lukasfels <lukas-fels@hotmail.de>
2022-09-13 22:09:17 +0200

    Update LearnView.vue

--- src/views/LearnView.vue
+++ src/views/LearnView.vue
@@ -42,6 +42,16 @@ function showInfo(index) {
     showModal.value = true
 }
 
+function getURL(mode, item) {
+    if (mode == 'flags') {
+        return new URL(import.meta.env.BASE_URL + "Flaggen/" + item.code + ".svg", import.meta.url).href
+    } else if (mode == 'outlines') {
+        return new URL(import.meta.env.BASE_URL + "Umrisse/" + item.continent + "/" + item.code + ".svg", import.meta.url).href
+    } else if (mode == 'pokemon') {
+        return new URL(import.meta.env.BASE_URL + "Pokemon/" + item.generation + "Generation/" + ('000' + item.id).slice(-3) + ".png", import.meta.url).href
+    }
+}
+
 startGame()
 </script>
     
@@ -51,19 +61,17 @@ startGame()
     <div class="learnItems">
         <div class="learnItem" @click="showInfo(index)" v-for="item, index in store.objQuestions">
             <span v-if="store.strMode == 'capitals'">{{item.name}}</span>
-            <img v-if="store.strMode == 'flags'" :src="'Flaggen/' + item.code + '.svg'" height="50">
-            <img v-if="store.strMode == 'outlines'" :src="'Umrisse/' + item.continent + '/' + item.code + '.svg'"
-                height="50">
-            <img v-if="store.strMode == 'pokemon'"
-                :src="'Pokemon/' + item.generation + 'Generation/' + ('000' + item.id).slice(-3) + '.png'" height="50">
+            <img v-if="store.strMode == 'flags'" :src="getURL('flags',item)" height="50">
+            <img v-if="store.strMode == 'outlines'" :src="getURL('outlines',item)" height="50">
+            <img v-if="store.strMode == 'pokemon'" :src="getURL('pokemon',item)" height="50">
         </div>
     </div>
-    <div class="modalView" :class="{showModal: !showModal}" @click="showModal = !showModal">
-        <div class="modalInfos">
+    <div class="modalView" :class="{showModal: !showModal}" @click="showModal = !showModal" v-if="showModal">
+        <div class="modalInfos" @click="showModal = true">
             <h1>{{store.objNext.name}}</h1>
             <p>Hauptstadt: {{store.objNext.capital}}</p>
-            <img :src="'Flaggen/' + store.objNext.code + '.svg'">
-            <img :src="'Umrisse/' + store.objNext.continent + '/' + store.objNext.code + '.svg'">
+            <img :src="getURL('flags',store.objNext)">
+            <img :src="getURL('outlines',store.objNext)">
         </div>
     </div>
 </template>
@@ -83,7 +91,7 @@ startGame()
 .modalView {
     background-color: rgba(0, 0, 0, 0.5);
     position: absolute;
-    z-index: 99;
+    z-index: 98;
     top: 0;
     left: 0;
     width: 100%;
@@ -93,6 +101,7 @@ startGame()
 
 .modalInfos {
     background-color: #2F2F2F;
+    z-index: 99;
     margin: auto;
     width: 80%;
     height: 80%;
